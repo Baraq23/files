1666495ac2123ffb05b39726879aaac0074d7ac3
barraotieno <barrackotieno160@gmail.com>
2024-05-15 16:42:34 +0300

    the program can now print the ascii art successfully. 'cat -e' error has been handle and now works perfectly

--- asciiArt/main/main.go
+++ asciiArt/main/main.go
@@ -2,46 +2,62 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
-    "fmt"
+	"strings"
 )
-
 
 func main() {
 	asciiMap := readBanner("standard.txt")
-	input := os.Args[1]
-	storeArt2(asciiMap, input)
+	input1 := os.Args[1:]
+	input := strings.Join(input1, " ")
+
+	// runes := []rune(input)
+	// fmt.Println(runes)
+
+	input = strings.ReplaceAll(input, "\n", "\\n")
+
+	words := strings.Split(input, "\\n")
+	for _, word := range words {
+		storeArt2(asciiMap, word)
+	}
 }
 
 // This function stores the ascii art to then prints the ascii art
 func storeArt2(asciiMap map[rune][]string, input string) {
-	//slice of a slice to store the Art
+	// slice of a slice to store the Art
 	store := [][]string{}
 	for _, chr := range input {
 		store = append(store, asciiMap[chr])
 	}
-	//Display what is in the store
+	// Hold the word in a slice
+	word := ""
 	for i := 0; i < 8; i++ {
-	 for j := 0; j < len(store); j++ {
-			fmt.Print(store[j][i])
+		for j := 0; j < len(store); j++ {
+			word += store[j][i]
+			// fmt.Print(store[j][i])
 		}
-		println()
+		if i != 7 {
+			word += "\n"
+		}
 	}
-} 
-//this function maps the ascii charracters to their corresponding art
+	// write the word now
+	fmt.Println(word)
+}
+
+// this function maps the ascii charracters to their corresponding art
 func readBanner(file string) map[rune][]string {
 	bannerFile, err := os.Open(file)
-
 	if err != nil {
 		panic(err)
 	}
 	defer bannerFile.Close()
 	scanner := bufio.NewScanner(bannerFile)
 	asciiMap := make(map[rune][]string)
-	
-	for i := 32; i <= 126; i++{
+
+	for i := 32; i <= 126; i++ {
 		runeValue := i
-		scanner.Scan() 
+		scanner.Scan()
 		var art []string
 		for i := 0; i < 8; i++ {
 			scanner.Scan()
@@ -49,7 +65,7 @@ func readBanner(file string) map[rune][]string {
 			art = append(art, line)
 		}
 		asciiMap[rune(runeValue)] = art
-		
+
 	}
 	return asciiMap
 }
